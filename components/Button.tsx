7833b5e13761e83211553ff7b8a1f04a99cbd9c9
dev-rome <haynesjerome@gmail.com>
2023-06-10 14:47:03 -0400

    added button type

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import Image from "next/image";
 import { ButtonProps } from "@/types";
 
-const Button = ({ title, containerStyles, handleClick  }: ButtonProps) => {
+const Button = ({
+  title,
+  containerStyles,
+  btnType,
+  handleClick,
+}: ButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>
-        {title}
-      </span>
+      <span className={`flex-1`}>{title}</span>
     </button>
   );
 };
