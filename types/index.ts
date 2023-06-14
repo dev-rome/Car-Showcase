4f32db58b38aa07ce1bb319da841742fca5e59c0
dev-rome <haynesjerome@gmail.com>
2023-06-14 16:36:55 -0400

    added car props and edit button props

--- types/index.ts
+++ types/index.ts
@@ -3,11 +3,29 @@ import { MouseEventHandler } from "react";
 export interface ButtonProps {
   title: string;
   containerStyles?: string;
+  textStyles?: string;
   btnType?: "button" | "submit";
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  year: number;
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
 }
