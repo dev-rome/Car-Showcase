5bb58941ab540e990890b00e859cc9f147f5f8e4
dev-rome <haynesjerome@gmail.com>
2023-06-14 16:39:28 -0400

    added to button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import Image from "next/image";
+
 import { ButtonProps } from "@/types";
 
 const Button = ({
   title,
   containerStyles,
+  textStyles,
   btnType,
   handleClick,
+  rightIcon,
+  isDisabled,
 }: ButtonProps) => {
   return (
     <button
@@ -15,7 +20,17 @@ const Button = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="Right Icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
