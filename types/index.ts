d240b32937fc2467fde6b5c8e1b859beedb9bee1
dev-rome <haynesjerome@gmail.com>
2023-06-15 15:58:12 -0400

    edit types for client side

--- types/index.ts
+++ types/index.ts
@@ -1,22 +1,6 @@
 import { MouseEventHandler } from "react";
 
-export interface ButtonProps {
-  title: string;
-  containerStyles?: string;
-  textStyles?: string;
-  btnType?: "button" | "submit";
-  handleClick?: MouseEventHandler<HTMLButtonElement>;
-  rightIcon?: string;
-  isDisabled?: boolean;
-}
-
-export interface SearchManufacturerProps {
-  manufacturer: string;
-  setManufacturer: (manufacturer: string) => void;
-}
-
 export interface CarProps {
-  year: number;
   city_mpg: number;
   class: string;
   combination_mpg: number;
@@ -28,6 +12,22 @@ export interface CarProps {
   make: string;
   model: string;
   transmission: string;
+  year: number;
+}
+
+export type CarState = CarProps[] & { message?: string };
+
+export interface SearchBarProps {
+  setManuFacturer: (manufacturer: string) => void;
+  setModel: (model: string) => void;
+}
+
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
 }
 
 export interface CarCardProps {
@@ -40,16 +40,14 @@ export interface CarCardProps {
   cityMPG: number;
 }
 
-export interface HomeProps {
-  searchParams: FilterProps;
-}
-
-export interface FilterProps {
-  manufacturer?: string;
-  year?: number;
-  fuel?: string;
-  limit?: number;
-  model?: string;
+export interface ButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface OptionProps {
@@ -57,17 +55,18 @@ export interface OptionProps {
   value: string;
 }
 
-export interface CustomFilterProps {
-  title: string;
+export interface CustomFilterProps<T> {
   options: OptionProps[];
+  setFilter: (selected: T) => void;
 }
 
 export interface ShowMoreProps {
   pageNumber: number;
   isNext: boolean;
+  setLimit: (limit: number) => void;
 }
 
 export interface SearchManuFacturerProps {
-  manufacturer: string;
-  setManuFacturer: (manufacturer: string) => void;
+  selected: string;
+  setSelected: (selected: string) => void;
 }
