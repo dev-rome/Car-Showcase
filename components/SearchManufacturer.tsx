af6278719b83dd3089d7f2b2e970bcc6a960dbd7
dev-rome <haynesjerome@gmail.com>
2023-06-15 14:59:02 -0400

    edit space

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -5,6 +5,7 @@ import { useState, Fragment } from "react";
 import Image from "next/image";
 
 import { Combobox, Transition } from "@headlessui/react";
+
 import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from "@/types";
 
